ded88ac6f4348a765292768fce889112b9ed1027
Gabi-Alex <gabi.alex.gor@gmail.com>
2022-08-07 21:53:21 +0300

    Update start-windows.bat

--- start-windows.bat
+++ start-windows.bat
@@ -10,14 +10,14 @@ rem hard-code a different path here or set it before calling this script.
 
 rem set python3_dir="C:\Custom\Path"
 if not defined python3_dir (
-	if not exist "c:\Python37" (
-	   echo "Downloading Python37..."
+	if not exist "c:\Python310" (
+	   echo "Downloading Python310..."
 	   powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12 ; wget https://www.python.org/ftp/python/3.7.1/python-3.7.1.exe -outfile python-3.7.1.exe"
 	   
-	   echo "Installing Python37..."
-	   start "Installing Python3.7.1 ..." /wait python-3.7.1.exe  /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Python37
+	   echo "Installing Python310..."
+	   start "Installing Python3.7.1 ..." /wait python-3.7.1.exe  /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Python310
 	)
-	set python3_dir=C:\Python37
+	set python3_dir=C:\Python310
 )
 
 rem ======================================================================
